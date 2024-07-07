8f1ee2d78e0ae6466fa87070b90d15524b310837
Favour Adebose <favouradebose@gmail.com>
2024-07-07 03:58:04 +0100

    edited vercel.json

--- authOrg/settings.py
+++ authOrg/settings.py
@@ -90,11 +90,11 @@ WSGI_APPLICATION = 'authOrg.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'NAME': 'DB_NAME',
+        'USER': 'DB_USER',
+        'PASSWORD':'DB_PASSWORD',
+        'HOST':'DB_HOST',
+        'PORT':'DB_PORT',
     }
 }
 AUTH_USER_MODEL = 'authapp.User'
