e90878c83774b49e61362bfeb6078a5252d68025
Favour Adebose <favouradebose@gmail.com>
2024-07-08 20:26:40 +0100

    made changes to both urls.py file and views.py file

--- authOrg/urls.py
+++ authOrg/urls.py
@@ -33,6 +33,6 @@ urlpatterns = [
     path('api/organisations/<str:orgId>', OrganisationDetailView.as_view(), name='organisation-detail'),
     path('api/organisations/<str:orgId>/users', AddUserToOrganisationView.as_view(), name='add-user-to-organisation'),
     path('api/organisations', UserOrganisationsView.as_view(), name='user-organisations'), 
-    path('api/organisations', CreateOrganisationView.as_view(), name='create-organisation') # New endpoint
+    path('api/organisations/', CreateOrganisationView.as_view(), name='create-organisation') # New endpoint
 
 ]
